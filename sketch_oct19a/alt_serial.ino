be9c1d78c20a491f885105910ab473b05c1d2533
kasp89s <skaspruk89@gmail.com>
2020-10-22 02:00:58 +0300

    Update alt_serial.ino

--- sketch_oct19a/alt_serial.ino
+++ sketch_oct19a/alt_serial.ino
@@ -17,3 +17,29 @@ void loop() {
       SIM800.write(Serial.read());
   }
 }
+
+#include <SoftwareSerial.h>
+
+//#include <AltSoftSerial.h>
+//AltSoftSerial SIM800;
+SoftwareSerial SIM800(8, 9);
+void setup() {
+  Serial.begin(9600);
+  Serial.println("1111!");
+  SIM800.begin(9600);
+  SIM800.println("AT");
+}
+
+void loop() {
+  // Определяем заряд батареи.
+  if (SIM800.available()){
+     int simResponse = SIM800.read();
+
+     Serial.write(simResponse);
+//     Serial.print(simResponse);
+  }
+  if (Serial.available()) {
+      SIM800.write(Serial.read());
+  }
+  delay(1000);
+}
